aeeae2f1973b1cf51636618730a290cd204954e6
Sylvia Hela Mullins <49255017+Helasrebellion@users.noreply.github.com>
2021-08-10 13:14:31 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Sylvia Mullins
+ */
+public class Calculator extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+        //sets icon in application
+        Image icon = new Image(getClass().getResourceAsStream("/Logo.png"));
+        stage.getIcons().add(icon);
+        //sets title in application
+        stage.setTitle("Sylvia's Basic Calculator");
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
